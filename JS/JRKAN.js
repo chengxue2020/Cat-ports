fc46b1c938ce46e5a935a5c367919a9372351750
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2023-04-28 11:15:19 +0800

    0

--- JS/JRKAN.js
+++ JS/JRKAN.js
@@ -11,14 +11,14 @@
 
 var rule = {
     title:'JRKAN直播',
-    host:'http://jrkankan.com',
-	// JRKAN备用域名:jrkan365.com / jrkankan.com / jryyds.com / jrsbxj.com
+    host:'http://m.jrskan8.com/',
+	// JRKAN备用域名:m.jrskan8.com / m.jrkan666.com / jryyds.com / jrsbxj.com
 	// JRKAN网址发布:qiumi1314.com
     url:'/fyclass',
     searchUrl:'',
     searchable:0,
     quickSearch:0,
-    class_name:'全部',
+    class_name:'JRS赛事直播',
     class_url:'/',
     //class_url:'?live',
     headers:{
@@ -31,9 +31,9 @@ var rule = {
     double:false,
     推荐:'*',
     // 一级播放线路x3 可自行切换
-    // 一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(0)&&href',//play.sportsteam365.com
+    //一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(0)&&href',//play.sportsteam365.com
     一级:'.loc_match:eq(2) ul;li:gt(1):lt(4)&&Text;img&&src;li:lt(2)&&Text;a:eq(1)&&href',//play.sportsteam333.com
-    // 一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(2)&&href',//play.sportsteam666.com
+    //一级:'.loc_match .d-touch;li&&Text;img&&src;.lab_time&&Text;a:eq(2)&&href',//play.sportsteam666.com
     二级:{title:'.sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text',img:'img&&src',desc:';;;.lab_team_home&&Text;.lab_team_away&&Text',content:'.sub_list ul&&Text',tabs:'',tab_text:'',lists:'.sub_channel a',list_text:'a&&Text',list_url:'a&&data-play'},
     搜索:'',
 }
